e7a14c0fe0dc5bbbfbe84260b3663c1d212766db
qwertychomp <97177478+qwertychomp@users.noreply.github.com>
2022-01-18 06:39:18 -0800

    Add files via upload
    adding game.py

---
+++ game.py
@@ -0,0 +1,27 @@
+import pygame 
+from pygame.locals import *
+pygame.init()
+screen = pygame.display.set_mode((700, 700))
+run = True
+px = 0
+py = 0
+class Player(pygame.sprite.Sprite):
+	def  __init__(self):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.image.load("player.png")	
+player1 = Player()
+while run:
+	for event in pygame.event.get():
+		keypress = pygame.key.get_pressed()
+		if keypress[pygame.K_RIGHT]:
+			px = px + 5
+		if keypress[pygame.K_LEFT]:
+			px = px - 5
+		if keypress[pygame.K_DOWN]:
+			py = py + 5
+		if keypress[pygame.K_UP]:
+			py = py - 5
+	screen.blit(player1.image, (px, py))
+	pygame.display.update()
+
+	
