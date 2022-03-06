566e9c16e1a8ea170fbbae74ffc1db9f1e8b4700
qwertychomp <97177478+qwertychomp@users.noreply.github.com>
2022-03-06 09:47:59 -0800

    added floor
    the wolf will not be able to walk through the floor

--- game.py
+++ game.py
@@ -6,11 +6,18 @@ run = True
 px = 0
 py = 0
 bg = pygame.image.load("assets/canvas.png")
+unscaledfloorimg = pygame.image.load("assets/floor.png")
+floorimg = pygame.transform.scale(unscaledfloorimg, (1400, 10)) 
 class Player(pygame.sprite.Sprite):
 	def  __init__(self):
 		pygame.sprite.Sprite.__init__(self)
-		self.image = pygame.image.load("assets/wolf.jpeg")	
+		self.image = pygame.image.load("assets/wolf.jpeg")
+class Ground(pygame.sprite.Sprite):
+	def  __init__(self):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = floorimg	
 player1 = Player()
+floor = Ground()
 while run:
 	screen.fill((0,0,0))
 	for event in pygame.event.get():
@@ -19,12 +26,13 @@ while run:
 			px = px + 5
 		if keypress[pygame.K_LEFT]:
 			px = px - 5
-		if keypress[pygame.K_DOWN]:
+		if keypress[pygame.K_DOWN] and py != 325:
 			py = py + 5
 		if keypress[pygame.K_UP]:
 			py = py - 5
 	screen.blit(bg, (0, 0))
 	screen.blit(player1.image, (px, py))
+	screen.blit(floor.image, (0, 450))
 	pygame.display.update()
 
 	
